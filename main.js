4071f1a49ea2f6706d47373862428f6e0060ccb9
chris0810 <you@example.com>
2023-05-25 17:44:49 +0200

    1

--- main.js
+++ main.js
@@ -35,4 +35,53 @@ async function showForecast(url, latlng) {
     let response = await fetch(url);
     let jsondata = await response.json();
     console.log(jsondata, latlng);
+
+    let timeseries= jsondata.properties.timeseries
+
+
+    let current = jsondata.properties.timeseries[0].data.instant.details;
+    console.log(current);
+    
+    let timestamp = new Date(jsondata.properties.meta.updated_at)
+    ();
+    
+    let markup = `<h4>Aktuelles Wetter für ${latlng.lat.toFixed(4)}, ${latlng.lng.toFixed(4)}</h4>;
+    <table>
+    <tr><td>Luftdruck (hPa)</td><td>${current.air_pressure_at_sea_level}</td></tr>
+    <tr><td>Lufttemperatur (°C)  </td><td>${current.air_temperature}</tr></tr>
+    <tr><td>Bewölkung (%) </td><td>${current.cloud_area_fraction}</tr></tr>
+    <tr><td>Relative Luftfeuchtigkeit (%) </td><td>${current.relative_humidity}</tr></tr>
+    <tr><td>Windrichtung (°)</tr> </td><td>${current.wind_from_direction}</tr></tr>
+    <tr><td>Windgeschwindigkeit (m/s) </td><td>${current.wind_speed}</tr></tr>
+   
+    </table>
+    `;
+
 }
+
+    for (let i=0; i<=24, i+=3;) {
+        let icon= timeseries[i].data.next_1_hours.summary.symbol_code;
+        let image= `icons/${icon}.svg`;
+        markup += `<img src=${image} style="width:32px;">`
+        console.log(icon, image);
+
+L.popup().setLatLng(latlng).setContent(markup).openOn(map);
+}
+
+map.on("click", function(evt) {
+    let url=`https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${evt.latlng.lat}&lon=${evt.latlng.lng}`
+        ;
+    showForecast(url, evt.latlng);
+
+});
+
+map.fireEvent("click",{
+    latlng: L.latlng(ibk.lat, ibk.lng)
+})
+
+async function loadWind(url){
+    let response= await fetch(url);
+    console.log(jsondata);
+
+}
+loadWind("https://geographie.uibk.ac.at/data/ecmwf/data/wind-10u-10v-europe.json")
